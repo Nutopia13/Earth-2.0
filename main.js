50671bc5003ebc0c063e374092c5993b6dee34c2
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-09-15 17:54:01 +0100

    Solved Mixed Content Issue

--- main.js
+++ main.js
@@ -129,30 +129,3 @@ btn.addEventListener("click", () => {
   img.classList.add("img_1");
 });
 
-//Rocket Button Animation
-const d = 40;
-document.querySelectorAll(".rocket-button").forEach((elem) => {
-  elem.querySelectorAll(".default").forEach((text) => {
-    charming(text);
-    text.querySelectorAll("span").forEach((span, i) => {
-      span.innerHTML = span.textContent == " " ? "&nbsp;" : span.textContent;
-      span.style.setProperty("--d", i * d + "ms");
-      span.style.setProperty(
-        "--ds",
-        text.querySelectorAll("span").length * d - d - i * d + "ms"
-      );
-    });
-  });
-  elem.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (elem.classList.contains("animated")) {
-      return;
-    }
-    elem.classList.add("animated");
-    elem.classList.toggle("live");
-    setTimeout(() => {
-      elem.classList.remove("animated");
-      elem.classList.toggle("live");
-    }, 3000);
-  });
-});
